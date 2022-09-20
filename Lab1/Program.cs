477e157211e41aa24fe8bc319c05563413238570
StadndartAccount <72969531+StadndartAccount@users.noreply.github.com>
2022-09-20 21:02:04 +0300

    origin

---
+++ Lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿using SFML.Graphics;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderWindow renderWindow = new RenderWindow(new SFML.Window.VideoMode(640, 320), "Test");
+            renderWindow.SetVerticalSyncEnabled(true);
+
+            while(renderWindow.IsOpen)
+            {
+                renderWindow.DispatchEvents();
+
+                renderWindow.Clear(Color.Black);
+
+                renderWindow.Display();
+            }
+        }
+    }
+}
