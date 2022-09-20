38f439a3bf252350117e866074eb468a7a3916f2
StadndartAccount <72969531+StadndartAccount@users.noreply.github.com>
2022-09-21 00:39:55 +0300

    bubbles

---
+++ Lab1/CustomCircle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.System;
+
+namespace Lab1
+{
+    public class CustomCircle
+    {
+        public CircleShape circle { get; private set; } = new CircleShape();
+        private float speed = 1;
+        private float resizingSpeed = 1;
+
+        public int radiusDirection = 1;
+        public float yDirection = 1;
+        public float xDirection = 1;
+        
+        public int minRadius { get; }
+        public int maxRadius { get; }
+        public float currentRadius
+        { 
+            get {
+                return circle.Radius;
+            }
+            set
+            {
+                circle.Radius = value;
+            }
+        }
+
+        public Vector2f currentPosition
+        {
+            get
+            {
+                return circle.Position;
+            }
+            set
+            {
+                circle.Position = value;
+            }
+        }
+
+        public CustomCircle()
+        {
+            Random rand = new Random();
+            minRadius = rand.Next(1, 20);
+            maxRadius = rand.Next(40, 50);
+            currentRadius = rand.Next(minRadius, maxRadius);
+
+            xDirection = (float)rand.NextDouble();
+            yDirection = (float)rand.NextDouble();
+
+            speed = rand.Next(1, 20) * (float)rand.NextDouble();
+            resizingSpeed = (maxRadius - minRadius) / 20;
+
+            circle.FillColor = new Color((byte)rand.Next(0, 255), (byte)rand.Next(0, 255), (byte)rand.Next(0, 255));
+            circle.OutlineThickness = rand.Next(1, 5);
+            circle.Position = new Vector2f(rand.Next(100, 300), rand.Next(50, 400));
+            circle.OutlineColor = new Color((byte)rand.Next(0, 255), (byte)rand.Next(0, 255), (byte)rand.Next(0, 255));
+
+        }
+
+        public void ChangeRadius()
+        {
+            currentRadius += radiusDirection * resizingSpeed;
+        }
+
+        public void ChangePosition()
+        {
+            Random rand = new Random();
+            currentPosition += new Vector2f(xDirection * (float)rand.NextDouble(), yDirection * (float)rand.NextDouble()) * speed;
+        }
+    }
+}
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using SFML.Graphics;
+using SFML.System;
+using System;
+using System.Collections.Generic;
 
 namespace Lab1
 {
@@ -6,14 +9,61 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            RenderWindow renderWindow = new RenderWindow(new SFML.Window.VideoMode(640, 320), "Test");
+            MainLoop();
+
+        }
+
+        static void MainLoop()
+        {
+            RenderWindow renderWindow = new RenderWindow(new SFML.Window.VideoMode(640, 640), "Test");
             renderWindow.SetVerticalSyncEnabled(true);
 
-            while(renderWindow.IsOpen)
+            uint windowWidth = renderWindow.Size.X;
+            uint windowHeight = renderWindow.Size.Y;
+
+
+            List<CustomCircle> circlesList = new List<CustomCircle>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                circlesList.Add(new CustomCircle());
+            }
+
+            while (renderWindow.IsOpen)
             {
                 renderWindow.DispatchEvents();
 
                 renderWindow.Clear(Color.Black);
+
+                foreach (CustomCircle circle in circlesList)
+                {
+                    Random rand = new Random();
+
+                    FloatRect bound = circle.circle.GetLocalBounds();
+                    circle.circle.Origin = new Vector2f(bound.Width / 2, bound.Height / 2);
+
+
+                    if (circle.currentRadius > circle.maxRadius && circle.radiusDirection > 0 || circle.currentRadius < circle.minRadius && circle.radiusDirection < 0)
+                    {
+                        circle.radiusDirection *= -1;
+                    }
+
+
+                    if (circle.currentPosition.X - circle.currentRadius < 0 || circle.currentPosition.X + circle.currentRadius > windowWidth)
+                    {
+                        circle.xDirection = -Math.Sign(circle.xDirection) * (float)rand.NextDouble();
+                    }
+
+                    if (circle.currentPosition.Y - circle.currentRadius < 0 || circle.currentPosition.Y + circle.currentRadius > windowHeight)
+                    {
+                        circle.yDirection = -Math.Sign(circle.yDirection) * (float)rand.NextDouble();
+                    }
+
+
+                    circle.ChangePosition();
+                    circle.ChangeRadius();
+                    renderWindow.Draw(circle.circle);
+                }
 
                 renderWindow.Display();
             }
